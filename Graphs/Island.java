355f3bc6ce3629dc66a8764ceac28fea7cf5586a
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2022-11-06 09:34:33 +0530

    Adding code for Island Problem

---
+++ Graphs/Island.java
@@ -0,0 +1,6 @@
+public class Island {
+    public static void main(String[] args) {
+        int adjMatrix [][] = { {0,1,1,0,0},{1,0,0,1,0},{1,0,0,0,1},{0,1,0,0,0},{0,0,1,0,0} };
+        int n = adjMatrix.length;
+    }
+}
