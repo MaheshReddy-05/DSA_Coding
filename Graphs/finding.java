89da0d7ff9adefb4054cf5d9f15e56bb5e6b2826
Mahesh Reddy <nagamaheshreddy3@gmail.com>
2022-11-23 21:29:54 +0530

    Leetcode Nov -21

---
+++ Graphs/finding.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class finding {
+    public static void main(String[] args) {
+        char [][] maze = {{'+','+','.','+'},{'.','.','.','+'},{'+','+','+','.'}};
+        int verify [][] = new int[maze.length][maze[0].length];
+        for(int[]i :verify){
+            Arrays.fill(i,-1);
+        }
+        int [] start = {1,2};
+        int level =0;
+        if(start[0]<=maze.length-1 && start[1]<=maze[0].length-1 && ( start[0]==maze.length-1 || start[1]==maze[0].length-1)) {level = 0;}
+        Queue<int[]>  q = new LinkedList<>();
+        q.add(start);
+
+        while(!q.isEmpty()){
+            level++;
+            int size = q.size();
+            for(int i =0;i<size;i++){
+                    
+            }
+        }
+    }
+}
