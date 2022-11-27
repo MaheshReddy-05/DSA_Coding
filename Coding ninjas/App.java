0d87c20fd7f3f2b8baa0613a6f273a69add552db
Mahesh Reddy <nagamaheshreddy3@gmail.com>
2022-11-27 21:24:31 +0530

    RM

---
+++ Coding ninjas/App.java
@@ -0,0 +1,12 @@
+// package Coding ninjas;
+public class App {
+    public static void main(String[] args) {
+        
+    }
+    public static void f(int n){
+        System.out.print(n+" ");
+        f(n-1);
+    }
+    
+}
+
