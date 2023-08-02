193eba971507f1e3917773abda660a6ff74da680
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-08-02 20:29:35 +0530

    LC: Strings

---
+++ Strings/Medium/2186. Minimum Number of Steps to Make Two Strings Anagram II.java
@@ -0,0 +1,32 @@
+// Author: Mahesh Reddy B N
+// Problem Link: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/description/
+
+
+class Solution {
+    public int minSteps(String s, String t) {
+        int sl = s.length();
+        HashMap<Character,Integer> hm = new HashMap<>();
+        int ans = s.length();
+        for(int i = 0;i<s.length();i++){
+            char curr = s.charAt(i);
+            hm.put(curr,hm.getOrDefault(curr,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char curr = t.charAt(i);
+            if(hm.containsKey(curr)){
+                ans--;
+                int cou = hm.get(curr);
+                if(cou==1){
+                    hm.remove(curr);
+                }
+                else{
+                    hm.put(curr,cou-1);
+                }
+            }
+            else{
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
