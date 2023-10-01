0d5bd92f09100bbea1825cd79f05eb56aee881dc
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-10-01 22:54:18 +0530

    LC

---
+++ LeetCode/Problems/Medium/11. Container With Most Water.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int si = 0;
+        int ei = height.length-1;
+        while(si<ei){
+            max = Math.max(max,(ei-si)*Math.min(height[si],height[ei]));
+            if(height[si]<height[ei]) si++;
+            else ei--;
+        }
+        return max;
+    }
+}
